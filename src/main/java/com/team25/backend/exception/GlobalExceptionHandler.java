791e6692a1c7f16cb0400fb3ffcbabd13826eeec
Kim Sanghae <pelikan@pusan.ac.kr>
2024-09-20 01:37:16 +0900

    feat(GlobalExceptionHandler): 예외 처리 handler 생성

---
+++ src/main/java/com/team25/backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.team25.backend.exception;
+
+import com.team25.backend.dto.response.ApiResponse;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ApiResponse<Map<String, String>>> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+            errors.put(error.getField(), error.getDefaultMessage()));
+        return new ResponseEntity<>(new ApiResponse<>(false, "유효성 검사 실패", errors),
+            HttpStatus.BAD_REQUEST);
+    }
+}

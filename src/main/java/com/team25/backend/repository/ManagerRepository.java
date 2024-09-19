3d0c339e8d7d9421b80d913dbeb863a2c67dde29
Kim Sanghae <pelikan@pusan.ac.kr>
2024-09-20 01:39:36 +0900

    feat($repository): findByUsername, findByWorkingRegion 메소드 추가

--- src/main/java/com/team25/backend/repository/ManagerRepository.java
+++ src/main/java/com/team25/backend/repository/ManagerRepository.java
@@ -1,10 +1,10 @@
 package com.team25.backend.repository;
 
 import com.team25.backend.entity.Manager;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ManagerRepository extends JpaRepository<Manager, Long> {
-    List<Manager> findByWorkingRegion(String workingRegion);
+
+  List<Manager> findByWorkingRegion(String region);
 }

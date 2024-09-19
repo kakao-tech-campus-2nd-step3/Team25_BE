8e1a47d1f60de3040f9f1c044c540e9303f1f3b6
Kim Sanghae <pelikan@pusan.ac.kr>
2024-09-20 01:34:21 +0900

    chore($entity): 파일 포매팅

--- src/main/java/com/team25/backend/entity/User.java
+++ src/main/java/com/team25/backend/entity/User.java
@@ -1,14 +1,24 @@
 package com.team25.backend.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import jakarta.persistence.*;
-
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Getter
 @Setter

66191a3308d0ccceeefe8b49aac95635262efe7f
Kim Sanghae <pelikan@pusan.ac.kr>
2024-09-20 01:35:38 +0900

    feat(Manager): Reservation 어트리뷰트 추가

--- src/main/java/com/team25/backend/entity/Manager.java
+++ src/main/java/com/team25/backend/entity/Manager.java
@@ -1,13 +1,25 @@
 package com.team25.backend.entity;
 
-import lombok.*;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity
 public class Manager {
 
@@ -18,14 +30,27 @@ public class Manager {
     @Column(length = 50)
     private String managerName;
 
-    private String profileImage;
-    private String career;
-    private String comment;
-    private String workingRegion;
-
     @OneToMany(mappedBy = "manager", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Certificate> certificates;
 
     @OneToMany(mappedBy = "manager", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<WorkingHour> workingHours;
+
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+
+    @Column(name = "profile_image", nullable = false)
+    private String profileImage;
+
+    @Column(name = "carrer", nullable = false, length = 256)
+    private String career;
+
+    @Column(name = "comment", nullable = false, length = 256)
+    private String comment;
+
+    @Column(name = "working_region", nullable = false, length = 256)
+    private String workingRegion;
+
+    @OneToMany(mappedBy = "manager")
+    private List<Reservation> reservations = new ArrayList<>();
 }

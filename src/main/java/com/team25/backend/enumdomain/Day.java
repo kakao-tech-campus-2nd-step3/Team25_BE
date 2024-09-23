ea51315338f4661f365ee1b25a3b295f7d8aa173
minjae <minjae4650@gmail.com>
2024-09-23 16:57:14 +0900

    feat: Day enum을 별도 파일로 분리
    Day enum을 WorkingHour 클래스에서 분리하여 enumdomain 패키지에 추가

---
+++ src/main/java/com/team25/backend/enumdomain/Day.java
@@ -0,0 +1,18 @@
+package com.team25.backend.enumdomain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Day {
+    MONDAY("월"),
+    TUESDAY("화"),
+    WEDNESDAY("수"),
+    THURSDAY("목"),
+    FRIDAY("금"),
+    SATURDAY("토"),
+    SUNDAY("일");
+
+    private final String krName;
+}

fa3522e8708f55c086729d32877cfaf344345c07
minjae <minjae4650@gmail.com>
2024-09-23 16:55:51 +0900

    feat: Manager 요소 중복 제거 및 초기화
    - name, managerName 중복 제거
    - new ArrayList로 초기화
    - import 간략화

--- src/main/java/com/team25/backend/entity/Manager.java
+++ src/main/java/com/team25/backend/entity/Manager.java
@@ -1,19 +1,9 @@
 package com.team25.backend.entity;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import lombok.*;
+import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
 @Setter
@@ -27,30 +17,20 @@ public class Manager {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long managerId;
 
-    @Column(length = 50)
+    @Column(length = 50, nullable = false)
     private String managerName;
 
-    @OneToMany(mappedBy = "manager", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Certificate> certificates;
-
-    @OneToMany(mappedBy = "manager", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<WorkingHour> workingHours;
-
-    @Column(name = "name", length = 50, nullable = false)
-    private String name;
-
-    @Column(name = "profile_image", nullable = false)
     private String profileImage;
-
-    @Column(name = "carrer", nullable = false, length = 256)
     private String career;
-
-    @Column(name = "comment", nullable = false, length = 256)
     private String comment;
-
-    @Column(name = "working_region", nullable = false, length = 256)
     private String workingRegion;
 
-    @OneToMany(mappedBy = "manager")
-    private List<Reservation> reservations = new ArrayList<>();
+    @OneToMany(mappedBy = "manager", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Certificate> certificates = new ArrayList<>();;
+
+    @OneToMany(mappedBy = "manager", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<WorkingHour> workingHours = new ArrayList<>();;
+
+    @OneToMany(mappedBy = "manager") // Reservation 관련은 삭제되면 안됨
+    private List<Reservation> reservations = new ArrayList<>();;
 }

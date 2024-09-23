6d486ec6dd6668dcac83b94c1bc00afd63a649dd
minjae <minjae4650@gmail.com>
2024-09-23 16:56:51 +0900

    refactor: Day enum을 별도 파일로 분리하여 WorkingHour 클래스 수정
    WorkingHour 클래스에서 Day enum을 enumdomain 패키지로 분리하고 import하여 사용하도록 수정

--- src/main/java/com/team25/backend/entity/WorkingHour.java
+++ src/main/java/com/team25/backend/entity/WorkingHour.java
@@ -1,17 +1,8 @@
 package com.team25.backend.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import jakarta.persistence.*;
+import com.team25.backend.enumdomain.Day;
 
 @Getter
 @Setter
@@ -33,24 +24,4 @@ public class WorkingHour {
     @ManyToOne
     @JoinColumn(name = "manager_id", nullable = false)
     private Manager manager;
-
-    public enum Day {
-        MONDAY("월"),
-        TUESDAY("화"),
-        WEDNESDAY("수"),
-        THURSDAY("목"),
-        FRIDAY("금"),
-        SATURDAY("토"),
-        SUNDAY("일");
-
-        private final String koreanName;
-
-        Day(String koreanName) {
-            this.koreanName = koreanName;
-        }
-
-        public String getKoreanName() {
-            return koreanName;
-        }
-    }
 }

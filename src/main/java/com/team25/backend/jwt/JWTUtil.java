312887c244149bb233992b920c32dd59daae0b0c
wvssm <neoramiregi@gmail.com>
2024-09-25 17:41:59 +0900

    feat(JWTUtil): 토큰에서 category 가져오는 로직 추가

--- src/main/java/com/team25/backend/jwt/JWTUtil.java
+++ src/main/java/com/team25/backend/jwt/JWTUtil.java
@@ -29,14 +29,19 @@ public class JWTUtil {
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
     }
 
+    public String getCategory(String token){
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("category", String.class);
+    }
+
     public Boolean isExpired(String token) {
 
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
     }
 
-    public String createJwt(String username, String role, Long expiredMs) {
+    public String createJwt(String category, String username, String role, Long expiredMs) {
 
         return Jwts.builder()
+                .claim("category", category)
                 .claim("username", username)
                 .claim("role", role)
                 .issuedAt(new Date(System.currentTimeMillis()))

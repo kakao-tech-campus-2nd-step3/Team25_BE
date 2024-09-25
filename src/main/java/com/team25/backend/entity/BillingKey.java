7367cd1e7bc2aeda4e04996cbe5622d1d6a9b3ed
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2024-09-25 16:53:35 +0900

    feat: lombok 사용하도록 수정

--- src/main/java/com/team25/backend/entity/BillingKey.java
+++ src/main/java/com/team25/backend/entity/BillingKey.java
@@ -1,7 +1,11 @@
 package com.team25.backend.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "billing_keys")
 public class BillingKey {
@@ -13,42 +17,4 @@ public class BillingKey {
     private String cardCode;
     private String cardName;
     private String userId; // 사용자 식별자 (예: 이메일, 아이디 등)
-
-    public Long getId() {
-        return id;
-    }
-
-    // ID는 자동 생성되므로 Setter는 만들지 않음
-
-    public String getBid() {
-        return bid;
-    }
-
-    public void setBid(String bid) {
-        this.bid = bid;
-    }
-
-    public String getCardCode() {
-        return cardCode;
-    }
-
-    public void setCardCode(String cardCode) {
-        this.cardCode = cardCode;
-    }
-
-    public String getCardName() {
-        return cardName;
-    }
-
-    public void setCardName(String cardName) {
-        this.cardName = cardName;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
 }

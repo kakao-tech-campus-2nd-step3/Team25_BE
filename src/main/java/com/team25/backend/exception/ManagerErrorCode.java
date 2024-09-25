6f3a147bd18409f0008ad3c71ead2cc0717b41f5
minjae <minjae4650@gmail.com>
2024-09-25 20:03:40 +0900

    feat: Manager 등록, 조회, 근무시간 등록 관련 ErrorCode 추가

--- src/main/java/com/team25/backend/exception/ManagerErrorCode.java
+++ src/main/java/com/team25/backend/exception/ManagerErrorCode.java
@@ -10,7 +10,10 @@ public enum ManagerErrorCode {
     INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "잘못된 입력 값입니다."),
     INVALID_DATE_FORMAT(HttpStatus.BAD_REQUEST, "날짜 형식이 옳지 않습니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "등록되지 않은 회원입니다."),
-    REGION_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 지역입니다.");
+    REGION_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 지역입니다."),
+    NAME_TOO_SHORT(HttpStatus.BAD_REQUEST, "이름은 1자 이상 입력해야 합니다."),
+    MANAGER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 매니저입니다."),
+    INVALID_WORKING_HOUR_FORMAT(HttpStatus.BAD_REQUEST, "근무 시작 시간은 숫자로 작성해야 합니다.");
 
     private final HttpStatus httpStatus;
     private final String message;

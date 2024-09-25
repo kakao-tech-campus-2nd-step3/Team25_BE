60265838adf2986e8b6282fecf5bace286d10c2a
minjae <minjae4650@gmail.com>
2024-09-25 20:03:22 +0900

    feat: Manager 등록, 조회, 근무시간 등록 Controller 추가

--- src/main/java/com/team25/backend/controller/ManagerController.java
+++ src/main/java/com/team25/backend/controller/ManagerController.java
@@ -1,15 +1,14 @@
 package com.team25.backend.controller;
 
-import com.team25.backend.dto.response.ApiResponse;
-import com.team25.backend.dto.response.ManagerByDateAndRegionResponse;
+import com.team25.backend.dto.request.ManagerCreateRequest;
+import com.team25.backend.dto.request.ManagerWorkingHourRequest;
+import com.team25.backend.dto.response.*;
 import com.team25.backend.service.ManagerService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -31,5 +30,49 @@ public class ManagerController {
             .build();
 
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/api/manager")
+    public ResponseEntity<ApiResponse<ManagerCreateResponse>> createManager(
+        @RequestBody ManagerCreateRequest request) {
+
+        ManagerCreateResponse response = managerService.createManager(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(ApiResponse.<ManagerCreateResponse>builder()
+                .status(true)
+                .message("매니저 등록을 성공했습니다.")
+                .data(response)
+                .build());
+    }
+
+    @GetMapping("/api/profile/{manager_id}")
+    public ResponseEntity<ApiResponse<ManagerProfileResponse>> getManagerProfile(
+        @PathVariable("manager_id") Long managerId) {
+
+        ManagerProfileResponse profileResponse = managerService.getManagerProfile(managerId);
+
+        return ResponseEntity.ok(
+            ApiResponse.<ManagerProfileResponse>builder()
+                .status(true)
+                .message("프로필 조회를 성공했습니다.")
+                .data(profileResponse)
+                .build()
+        );
+    }
+
+    @PostMapping("/api/manager/time/{manager_id}")
+    public ResponseEntity<ApiResponse<ManagerWorkingHourResponse>> addWorkingHour(
+        @PathVariable("manager_id") Long managerId,
+        @RequestBody ManagerWorkingHourRequest request) {
+
+        ManagerWorkingHourResponse response = managerService.addWorkingHour(managerId, request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(ApiResponse.<ManagerWorkingHourResponse>builder()
+                .status(true)
+                .message("근무 시간을 성공적으로 등록했습니다.")
+                .data(response)
+                .build());
     }
 }

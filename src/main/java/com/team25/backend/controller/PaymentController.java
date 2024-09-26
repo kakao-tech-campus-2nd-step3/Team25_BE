e789fd613746da69698b8446eede4ee0c1142ced
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2024-09-26 23:04:40 +0900

    refactor: 응답 형식 통일화

--- src/main/java/com/team25/backend/controller/PaymentController.java
+++ src/main/java/com/team25/backend/controller/PaymentController.java
@@ -4,10 +4,12 @@ import com.team25.backend.dto.CustomUserDetails;
 import com.team25.backend.dto.request.BillingKeyRequest;
 import com.team25.backend.dto.request.PaymentRequest;
 import com.team25.backend.dto.request.ExpireBillingKeyRequest;
+import com.team25.backend.dto.response.ApiResponse;
 import com.team25.backend.dto.response.BillingKeyResponse;
 import com.team25.backend.dto.response.PaymentResponse;
 import com.team25.backend.dto.response.ExpireBillingKeyResponse;
 import com.team25.backend.service.PaymentService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -25,34 +27,42 @@ public class PaymentController {
 
     // 빌링키 발급
     @PostMapping("/billing-key")
-    public ResponseEntity<BillingKeyResponse> createBillingKey(@RequestBody BillingKeyRequest requestDto) throws Exception {
+    public ResponseEntity<ApiResponse<BillingKeyResponse>> createBillingKey(@RequestBody BillingKeyRequest requestDto) throws Exception {
         String userId = getCurrentUserId(); // 현재 사용자 식별자 가져오기
         BillingKeyResponse responseDto = paymentService.createBillingKey(requestDto, userId);
-        return ResponseEntity.ok(responseDto);
+        return new ResponseEntity<>(
+                new ApiResponse<>(true, "빌링키 발급을 성공했습니다.", responseDto), HttpStatus.OK
+        );
     }
 
     // 결제 요청
     @PostMapping("/payment")
-    public ResponseEntity<PaymentResponse> payment(@RequestBody PaymentRequest requestDto) throws Exception {
+    public ResponseEntity<ApiResponse<PaymentResponse>> payment(@RequestBody PaymentRequest requestDto) throws Exception {
         String userId = getCurrentUserId();
         PaymentResponse responseDto = paymentService.requestPayment(userId, requestDto);
-        return ResponseEntity.ok(responseDto);
+        return new ResponseEntity<>(
+                new ApiResponse<>(true, responseDto.resultMsg(), responseDto), HttpStatus.OK
+        );
     }
 
     // 빌링키 삭제
     @PostMapping("/billing-key/expire")
-    public ResponseEntity<ExpireBillingKeyResponse> expireBillingKey(@RequestBody ExpireBillingKeyRequest requestDto) throws Exception {
+    public ResponseEntity<ApiResponse<ExpireBillingKeyResponse>> expireBillingKey(@RequestBody ExpireBillingKeyRequest requestDto) throws Exception {
         String userId = getCurrentUserId();
         ExpireBillingKeyResponse responseDto = paymentService.expireBillingKey(userId, requestDto);
-        return ResponseEntity.ok(responseDto);
+        return new ResponseEntity<>(
+                new ApiResponse<>(true, responseDto.resultMsg(), responseDto), HttpStatus.OK
+        );
     }
 
     // 빌링키 존재 유무 확인
     @GetMapping("/billing-key/exists")
-    public ResponseEntity<Boolean> billingKeyExists() {
+    public ResponseEntity<ApiResponse<Boolean>> billingKeyExists() {
         String userId = getCurrentUserId();
         boolean exists = paymentService.billingKeyExists(userId);
-        return ResponseEntity.ok(exists);
+        return new ResponseEntity<>(
+                new ApiResponse<>(true, "성공적으로 빌링키 존재 유무를 가져왔습니다.", exists), HttpStatus.OK
+        );
     }
 
     private String getCurrentUserId() {

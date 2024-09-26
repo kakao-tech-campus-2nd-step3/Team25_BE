7b0a92396e0fda127f5d17ab5d673a98a711fa25
minjae <minjae4650@gmail.com>
2024-09-26 11:30:49 +0900

    feat: 매니저 사진 변경 DTO 생성

---
+++ src/main/java/com/team25/backend/dto/request/ManagerProfileImageUpdateRequest.java
@@ -0,0 +1,10 @@
+package com.team25.backend.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ManagerProfileImageUpdateRequest {
+    private String profileImage;
+}

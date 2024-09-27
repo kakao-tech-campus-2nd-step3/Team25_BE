81fd56a4f98dde599d359ffd94b761ecc80598cf
minjae <minjae4650@gmail.com>
2024-09-27 16:07:24 +0900

    refactor: record로 수정

--- src/main/java/com/team25/backend/dto/response/ManagerProfileImageUpdateResponse.java
+++ src/main/java/com/team25/backend/dto/response/ManagerProfileImageUpdateResponse.java
@@ -1,21 +1,9 @@
 package com.team25.backend.dto.response;
 
 import com.team25.backend.entity.Manager;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.AllArgsConstructor;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@Builder
-public class ManagerProfileImageUpdateResponse {
-    private String profileImage;
-
+public record ManagerProfileImageUpdateResponse(String profileImage) {
     public static ManagerProfileImageUpdateResponse fromEntity(Manager manager) {
-        return ManagerProfileImageUpdateResponse.builder()
-            .profileImage(manager.getProfileImage())
-            .build();
+        return new ManagerProfileImageUpdateResponse(manager.getProfileImage());
     }
 }

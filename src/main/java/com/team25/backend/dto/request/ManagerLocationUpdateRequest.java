4ff8d2f266fc18d6902586bd4b8c4c94c3560773
minjae <minjae4650@gmail.com>
2024-09-27 15:53:42 +0900

    refactor: record로 수정

--- src/main/java/com/team25/backend/dto/request/ManagerLocationUpdateRequest.java
+++ src/main/java/com/team25/backend/dto/request/ManagerLocationUpdateRequest.java
@@ -1,10 +1,4 @@
 package com.team25.backend.dto.request;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class ManagerLocationUpdateRequest {
-    private String workingRegion;
+public record ManagerLocationUpdateRequest(String workingRegion) {
 }

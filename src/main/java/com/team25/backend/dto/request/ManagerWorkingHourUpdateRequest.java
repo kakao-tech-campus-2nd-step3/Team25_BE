ac0def936c6cf3647060ec4dddf05cd678ccfe82
minjae <minjae4650@gmail.com>
2024-09-27 15:59:03 +0900

    refactor: record로 수정

--- src/main/java/com/team25/backend/dto/request/ManagerWorkingHourUpdateRequest.java
+++ src/main/java/com/team25/backend/dto/request/ManagerWorkingHourUpdateRequest.java
@@ -1,12 +1,8 @@
 package com.team25.backend.dto.request;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class ManagerWorkingHourUpdateRequest {
-    private String day;
-    private String startTime;
-    private String endTime;
+public record ManagerWorkingHourUpdateRequest (
+    String day,
+    String startTime,
+    String endTime
+) {
 }

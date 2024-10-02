f866c5e0c159e3cd0621104dbf129a3f5c029aaa
unknown <p.plue1881@gmail.com>
2024-10-02 16:13:57 +0900

    feat: 상점 번호로 결제정보 가져오기

--- src/main/java/com/team25/backend/repository/PaymentRepository.java
+++ src/main/java/com/team25/backend/repository/PaymentRepository.java
@@ -3,5 +3,8 @@ package com.team25.backend.repository;
 import com.team25.backend.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<Payment> findByOrderId(String orderId);
 }

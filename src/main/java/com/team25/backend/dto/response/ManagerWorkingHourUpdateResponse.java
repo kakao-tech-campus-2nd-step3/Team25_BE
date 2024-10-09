ddb7c4a583d3c6aedd2a977afcb9ffabc19ac498
minjae <minjae4650@gmail.com>
2024-10-10 04:13:22 +0900

    feat: 변경했을 때 Response 수정

--- src/main/java/com/team25/backend/dto/response/ManagerWorkingHourUpdateResponse.java
+++ src/main/java/com/team25/backend/dto/response/ManagerWorkingHourUpdateResponse.java
@@ -3,15 +3,23 @@ package com.team25.backend.dto.response;
 import com.team25.backend.entity.WorkingHour;
 
 public record ManagerWorkingHourUpdateResponse(
-    String day,
-    String startTime,
-    String endTime
+    String monStartTime, String monEndTime,
+    String tueStartTime, String tueEndTime,
+    String wedStartTime, String wedEndTime,
+    String thuStartTime, String thuEndTime,
+    String friStartTime, String friEndTime,
+    String satStartTime, String satEndTime,
+    String sunStartTime, String sunEndTime
 ) {
     public static ManagerWorkingHourUpdateResponse fromEntity(WorkingHour workingHour) {
         return new ManagerWorkingHourUpdateResponse(
-            workingHour.getDayOfWeek().getKrName(),
-            workingHour.getStartTime(),
-            workingHour.getEndTime()
+            workingHour.getMonStartTime(), workingHour.getMonEndTime(),
+            workingHour.getTueStartTime(), workingHour.getTueEndTime(),
+            workingHour.getWedStartTime(), workingHour.getWedEndTime(),
+            workingHour.getThuStartTime(), workingHour.getThuEndTime(),
+            workingHour.getFriStartTime(), workingHour.getFriEndTime(),
+            workingHour.getSatStartTime(), workingHour.getSatEndTime(),
+            workingHour.getSunStartTime(), workingHour.getSunEndTime()
         );
     }
 }

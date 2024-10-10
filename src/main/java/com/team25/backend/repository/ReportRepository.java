ab37a09af392f74f6e02b7a5c08ebb7477b78a1c
Kim Sanghae <pelikan@pusan.ac.kr>
2024-10-10 22:03:58 +0900

    feat(ReportRepository): 예약 ID로 리포트 조회

--- src/main/java/com/team25/backend/repository/ReportRepository.java
+++ src/main/java/com/team25/backend/repository/ReportRepository.java
@@ -1,6 +1,7 @@
 package com.team25.backend.repository;
 
 import com.team25.backend.entity.Report;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -9,6 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Long>,
     JpaSpecificationExecutor<Report> {
-    public Optional<Report> findByReservation_Id(Long reservation);
+    public Optional<List<Report>> findByReservation_Id(Long reservation);
 
 }

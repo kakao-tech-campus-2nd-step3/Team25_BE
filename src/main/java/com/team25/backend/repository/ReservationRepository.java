94158f68cb02683809463823f23812ce2923809c
Kim Sanghae <pelikan@pusan.ac.kr>
2024-10-10 19:03:49 +0900

    feat(ReservationRepository): 사용자 UUID를 통한 예약 리스트 조회 추가

--- src/main/java/com/team25/backend/repository/ReservationRepository.java
+++ src/main/java/com/team25/backend/repository/ReservationRepository.java
@@ -1,6 +1,8 @@
 package com.team25.backend.repository;
 
 import com.team25.backend.entity.Reservation;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -8,5 +10,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long>,
     JpaSpecificationExecutor<Reservation> {
-
+    Optional<List<Reservation>> findByUser_Uuid(String userUuid);
 }

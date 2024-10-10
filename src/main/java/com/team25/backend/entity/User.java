4f502aff6cab682e029311a84be5f92387b9ddb4
Kim Sanghae <pelikan@pusan.ac.kr>
2024-10-10 19:04:21 +0900

    refactor(User): 파일 포매팅

--- src/main/java/com/team25/backend/entity/User.java
+++ src/main/java/com/team25/backend/entity/User.java
@@ -2,26 +2,19 @@ package com.team25.backend.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.experimental.SuperBuilder;
-
-import java.util.UUID;
 
 @NoArgsConstructor
 @AllArgsConstructor

3f816b0cd169495fdbd80b4facf03ea89716eca6
wvssm <neoramiregi@gmail.com>
2024-10-11 16:29:17 +0900

    refactor(CustomUserDetailsService): setter 대신 생성자를 사용하도록 수정

--- src/main/java/com/team25/backend/service/CustomUserDetailsService.java
+++ src/main/java/com/team25/backend/service/CustomUserDetailsService.java
@@ -2,7 +2,6 @@ package com.team25.backend.service;
 
 import com.team25.backend.dto.CustomUserDetails;
 import com.team25.backend.entity.User;
-import com.team25.backend.exception.UserNotFoundException;
 import com.team25.backend.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -25,11 +24,8 @@ public class CustomUserDetailsService implements UserDetailsService {
         return userRepository.findByUsername(username)
                 .map(CustomUserDetails::new)
                 .orElseGet(() -> {
-                    User newUser = new User();
-                    String userUUID = UUID.randomUUID().toString();
-                    newUser.setUsername(username);
-                    newUser.setUuid(userUUID);
-                    newUser.setRole("ROLE_USER");
+                    String newUserUUID = UUID.randomUUID().toString();
+                    User newUser = new User(username, "ROLE_USER", newUserUUID);
                     userRepository.save(newUser);
                     return new CustomUserDetails(newUser);
                 });

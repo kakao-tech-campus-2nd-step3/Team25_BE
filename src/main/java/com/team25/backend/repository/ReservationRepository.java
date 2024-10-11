6c4f5c692e829c83d4c6cfb9f3e99a80be38de89
Kim Sanghae <pelikan@pusan.ac.kr>
2024-10-11 12:47:39 +0900

    fix($repository): Optional<List>는 List타입으로 수정

--- src/main/java/com/team25/backend/repository/ReservationRepository.java
+++ src/main/java/com/team25/backend/repository/ReservationRepository.java
@@ -2,7 +2,6 @@ package com.team25.backend.repository;
 
 import com.team25.backend.entity.Reservation;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -10,5 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long>,
     JpaSpecificationExecutor<Reservation> {
-    Optional<List<Reservation>> findByUser_Uuid(String userUuid);
+    List<Reservation> findByUser_Uuid(String userUuid);
 }
